6f63636973346676209ebfee7c1140dd17229f7d
SidTDev <sid.limitless@gmail.com>
2022-02-24 23:02:47 +0000

    Added spawning an object, attachment point objects
    Added the capacity to spawn objects from a prefab, and some attachment points to serve as the basis of an attachment system

--- OS and Concurrency game v1/Assets/GameScene1/Scripts/GameScript.cs
+++ OS and Concurrency game v1/Assets/GameScene1/Scripts/GameScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    [SerializeField] private GameObject taskPrefab; //defined in editor
+    private GameObject processQueue;
+    private
     // Start is called before the first frame update
     void Start()
     {
-        
+        processQueue = GameObject.Find("ProcessBox");
+        Instantiate(taskPrefab, new Vector3(1.0F, 0, 0) + processQueue.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
