68a67e6759b548fd0eb0d814de86c1fe4906718b
SidTDev <sid.limitless@gmail.com>
2022-03-02 23:27:41 +0000

    Implemented positioned task spawning
    Tasks now spawn in the correct places in the process queue area, wherever the next free space is available (task spawn bound to up arrow key for testing)
    Also improved attachment point code, no longer allows multiple tasks at the same point

--- OS and Concurrency game v1/Assets/GameScene1/Scripts/AttachPointScript.cs
+++ OS and Concurrency game v1/Assets/GameScene1/Scripts/AttachPointScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AttachPointScript : MonoBehaviour
 {
-    private BoxCollider2D attachCollider;
+    private BoxCollider2D attachCollider; //references the collider component
+    public GameObject attachedTask = null; //stores the attached task
 
     // Start is called before the first frame update
     void Start()
@@ -15,22 +16,39 @@ public class AttachPointScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //whenever the task is not being dragged, set it's pos to the attach point
+        if (attachedTask != null)
+        {
+            if (attachedTask.GetComponent<TaskScript>().dragging == false)
+            {
+                attachedTask.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+            }
+        }
         
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log("sas");
-        
-        if (other.name.Contains("Task")) //if the touching object name contains "task", any cloned task should have this property
+        addTask(collision.transform.gameObject);
+    }
+
+    public void addTask(GameObject task)
+    {
+        if (task.name.Contains("Task")) //if the object name contains "task", any cloned task should have this property
         {
-        //is it efficient to make this check every frame? no
-        //will i change it if i dont need to? no lol
-            if (other.GetComponent<TaskScript>().dragging == false)
+            //i had to change the check :((
+            if (attachedTask == null)
             {
-                Debug.Log("yoy");
-                other.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+                attachedTask = task;
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject == attachedTask)
+        {
+            attachedTask = null;
         }
     }
 }
--- OS and Concurrency game v1/Assets/GameScene1/Scripts/GameScript.cs
+++ OS and Concurrency game v1/Assets/GameScene1/Scripts/GameScript.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class GameScript : MonoBehaviour
 {
     [SerializeField] private GameObject taskPrefab; //defined in editor
-    private GameObject processQueue;
-    
+    private GameObject processQueue; //references the process queue object
+    private List<GameObject> pqAttachPoints; //references the process queue's attachment points
+
     // Start is called before the first frame update
     void Start()
     {
-        processQueue = GameObject.Find("ProcessBox");
-        Instantiate(taskPrefab, new Vector3(1.0F, 0, 0) /*+ processQueue.transform.position*/, Quaternion.identity);
+        processQueue = GameObject.Find("ProcessBox"); //get the process queue 
+        foreach (Transform child in processQueue.transform) //get all the attachment points of the PQ, put in a list
+        {
+            pqAttachPoints.Add(child.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //keybind for testing purposes
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            spawnTask();
+        }
+    }
+
+    //add a task in the next valid position in the process queue
+    void spawnTask()
+    {
+        foreach (GameObject ap in pqAttachPoints)
+        {
+            if (ap.GetComponent<AttachPointScript>().attachedTask == null)
+            {
+                ap.GetComponent<AttachPointScript>().addTask(Instantiate(taskPrefab, ap.transform.position, Quaternion.identity));
+                return; //this probably isnt good code practice idk
+            }
+        }
     }
 }
