9375f7d69cec254e3874a33887a8daaa1431805d
k20064897 <K20064897@kcl.ac.uk>
2022-03-11 13:44:28 +0000

    Add belt animation

--- OS and Concurrency game v1/Assets/GameScene1/Scripts/BeltScript.cs
+++ OS and Concurrency game v1/Assets/GameScene1/Scripts/BeltScript.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ This script animates texture applied on an object in a specified direction/offset
+*/
+
 public class BeltScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Scroll texture based on time 
+
+    public float scrollSpeedX = 0.5f, scrollSpeedY = 0.5f;
+    Renderer rend;
+
     void Start()
     {
-        
+        rend = GetComponent<Renderer> ();
+        rend.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float offsetX = Time.time * scrollSpeedX;
+        float offsetY = Time.time * scrollSpeedY;
+        rend.material.mainTextureOffset = new Vector2(offsetX, offsetY);
     }
 }
