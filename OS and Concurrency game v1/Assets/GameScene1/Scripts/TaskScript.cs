7fbca5134da2f6072a83717d3a17f11fdc842080
Suliman <suliman.riaz@kcl.ac.uk>
2022-03-13 19:57:16 +0000

    Added properties to task
    added burst time and priority

--- OS and Concurrency game v1/Assets/GameScene1/Scripts/TaskScript.cs
+++ OS and Concurrency game v1/Assets/GameScene1/Scripts/TaskScript.cs
@@ -7,16 +7,35 @@ public class TaskScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
-//Changing the object color to be blue when mouse move over
+    //Changing the object color to be blue when mouse move over
     private Color mouseOverColor = Color.blue;
     private Color originalColor = Color.yellow;
     private bool dragging = false;
     private float distance;
- 
-    //
+
+
+    private int burst_time;
+    private int priority;
+
+    public void Set_priority(int set_priority)
+
+    {
+        priority = set_priority;
+    }
+
+
+    public void Set_burst_time(int set_burst_time)
+
+    {
+        burst_time = set_burst_time;
+    }
+
+
+
     void OnMouseEnter()
     {
         GetComponent<Renderer>().material.color = mouseOverColor;
